acc0507e3890a41da59b18abf69493a623ac89d9
Michal Futrega <mfutrega@nvidia.com>
2025-01-23 16:14:46 +0100

    Add fused swiglu for MLP

--- megatron/core/models/gpt/gpt_layer_specs.py
+++ megatron/core/models/gpt/gpt_layer_specs.py
@@ -31,6 +31,7 @@ try:
         TELayerNormColumnParallelLinear,
         TENorm,
         TERowParallelLinear,
+        TESwigluLinear
     )
 
     HAVE_TE = True
@@ -137,6 +138,9 @@ def get_gpt_layer_with_transformer_engine_spec(
                 pre_mlp_layernorm=TENorm if num_experts else IdentityOp,
                 mlp=mlp,
                 mlp_bda=get_bias_dropout_add,
+                sharded_state_dict_keys_map={
+                    'mlp.swiglu_fc2.1.basic_ops.0.weight': 'mlp.linear_fc2.weight',
+                },
             ),
         )
 
@@ -272,6 +276,7 @@ def get_mlp_module_spec(
             submodules=MLPSubmodules(
                 linear_fc1=TELayerNormColumnParallelLinear if use_te else ColumnParallelLinear,
                 linear_fc2=TERowParallelLinear if use_te else RowParallelLinear,
+                swiglu_fc2=TESwigluLinear if use_te else RowParallelLinear,
             ),
         )
     else:
